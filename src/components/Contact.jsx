45cb118702da2c6af43a90cb8c3c4eb0928f7417
nethajipersonal <161015791+nethajipersonal@users.noreply.github.com>
2025-02-09 23:56:57 +0530

    Update Contact.jsx
    chnages4

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,8 @@ const Contact = () => {
                 >
                     {CONTACT.phoneNo}
                 </motion.p>
-                <a href="" className="border-b">{CONTACT.email}</a>
+                <a href={`mailto:${CONTACT.email}`} className="border-b">{CONTACT.email}</a>
+
             </div>
         </div>
     )
